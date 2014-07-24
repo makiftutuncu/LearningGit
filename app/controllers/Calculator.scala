47a78f33e386a62d29a86469ab3378d7d50d7be9
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2014-07-24 12:50:27 +0300

    Added a new multiply

--- app/controllers/Calculator.scala
+++ app/controllers/Calculator.scala
@@ -6,7 +6,11 @@ import play.api.mvc.{Action, Controller}
  * User: mehmetakiftutuncu - 24/07/14 - 12:21
  */
 object Calculator extends Controller {
-  def calculate(i: Int, j: Int) = Action {
+  def sum(i: Int, j: Int) = Action {
     Ok(s"Result is ${i + j}")
+  }
+
+  def multiply(i: Int, j: Int) = Action {
+    Ok(s"Result is ${i * j}")
   }
 }
